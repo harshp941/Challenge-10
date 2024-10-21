d834129aea02de40b3995ae5797d6eea8e2ec19b
Harsh Patel <harshp5@usf.edu>
2024-10-20 20:39:30 -0400

    Handle stock availability and disable purchase button when out of stock

--- product_managment.js
+++ product_managment.js
@@ -1,8 +1,19 @@
 //Task 2 Add Event Listeners for Product Selection
 const priceElement = document.getElementById("product-price");
-const sizeSelector = document.getElementById("size-selector")
+const sizeSelector = document.getElementById("size-selector");
+const purchaseButton = document.getElementById("purchase-button")
 
 sizeSelector.addEventListener("change" , (event) => {
     const selectedPrice = event.target.value;
-    priceElement.textContent = `$${selectedPrice}`
-})
+    priceElement.textContent = `$${selectedPrice}`;
+// Task 3 Handle Stock Availability
+
+if ("data-stock" === "out-of-stock") {
+    purchaseButton.disabled = true;
+} else {
+    purchaseButton.disabled = false;
+}
+});
+
+// Task 4 Create a Checkout Event
+
